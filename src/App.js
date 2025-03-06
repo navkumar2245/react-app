4d20f483ecc6a9db99bdec4f4ebc4af32bb59318
navkumar2245 <42208401+navkumar2245@users.noreply.github.com>
2025-03-06 22:48:02 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+import UserData from "./UserData";
+
+function App() {
+  return (
+    <Container maxwidth="md" sx={{ mt: 8 }}>
+      <UserData/>
+      </Container>
+
+  );
+}
+
+export default App;
